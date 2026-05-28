e519eb22bfc89c687cd451480386b09fc94edc2b
Christian <christian.cuneo@libero.it>
2026-05-28 18:00:20 +0200

    unignored fastify.d.ts file

---
+++ packages/backend/types/fastify.d.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+import type { SignOptions } from 'jsonwebtoken'
+declare module 'fastify' {
+    interface FastifyInstance {
+        prisma: PrismaClient
+        customGenerateToken : (payload : object) => string
+    }
+    interface FastifyRequest {
+        user: {
+            id : number
+        }
+    }
+}
