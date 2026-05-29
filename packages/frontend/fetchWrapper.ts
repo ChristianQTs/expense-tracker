924fb1d5d46603ebdb1eb005e7996390171f0737
Christian <christian.cuneo@libero.it>
2026-05-29 09:00:19 +0200

    Fixed URL import.meta and added frontend tsconfig.json

--- packages/frontend/fetchWrapper.ts
+++ packages/frontend/fetchWrapper.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://expense-tracker-production-776a.up.railway.app:8001/users'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5173'
 interface Options extends Omit<RequestInit, 'body'> {
     body?:any
 }
