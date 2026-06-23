4db46fe43e300aa3fba7a885405ac380cb4a8d7f
Christian <christian.cuneo@libero.it>
2026-06-23 20:13:14 +0200

    Added rate-limiting

--- packages/backend/Fastify API/app.ts
+++ packages/backend/Fastify API/app.ts
@@ -3,13 +3,26 @@ import Fastify from 'fastify'
 import type { FastifyError } from 'fastify'
 import cookie from '@fastify/cookie'
 import cors from '@fastify/cors'
+import fastifyRateLimit from '@fastify/rate-limit'
+import {Redis} from 'ioredis' 
 import { expensesRoutes } from './routes/expensesRoutes.js'
 import { budgetRoutes } from './routes/budgetRoutes.js'
 import { authRoutes } from './routes/authRoutes.js'
 
+const redisRateLimiting = new Redis(process.env.REDIS_URL || 'redis://127.0.0.1:6379', {
+    maxRetriesPerRequest: null,
+    connectTimeout: 5000
+})
+
 const port : number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8001
-const app = Fastify({ logger: false })
+const app = Fastify({ logger: false, trustProxy:true })
 await app.register(cookie, { secret: process.env.JWT_SECRET })
+await app.register(fastifyRateLimit, {
+    max: 100,
+    timeWindow: 60000,
+    redis: redisRateLimiting,
+    errorResponseBuilder: (request, context) => ({ statusCode: 429, error: 'Too many request', message:`Too much expense tracking, try again in ${context.after}`})
+})
 await app.register(cors, {
     origin: (origin, cb) => {
         // Allow local development or requests with no origin (like mobile apps/Postman)
@@ -20,7 +33,7 @@ await app.register(cors, {
 
         const hostname = new URL(origin).hostname
 
-        // Check if the domain is your production URL OR a dynamic Vercel sub-domain
+        // Check if the domain is production URL OR a dynamic Vercel sub-domain
         if (
             hostname === 'expense-tracker-qt.vercel.app' ||
             hostname.endsWith('.vercel.app')
